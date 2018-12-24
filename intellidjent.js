f752413fc4307a42167f31eda75986b8186ecd88
jeffreyyang3 <jeffreyhasmail@gmail.com>
2018-12-23 22:43:42 -0800

    sound files

--- intellidjent.js
+++ intellidjent.js
@@ -1,1 +1,50 @@
 console.log("linked")
+
+
+let app = new Vue({
+		el: '#app',
+		data: {
+				dummy: "asdf"
+		}
+})                      
+
+
+
+let x = new Howl({
+		    src: ['sounds/acoustic-snare.mp3'],
+			html5: true
+})
+console.log("asdf")
+console.log(document.getElementById("test"))
+document.addEventListener("click", function(){
+		x.play()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
