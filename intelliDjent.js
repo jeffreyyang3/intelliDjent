139f1f7ddeef3a5ad1a4125763708791a3c3380c
Jeffrey Yang <jeffreyhasmail@gmail.com>
2018-12-27 00:56:11 +0000

    hammer on pull off

--- intelliDjent.js
+++ intelliDjent.js
@@ -9,47 +9,62 @@ let app = new Vue({
 	}
 })
 
+setHowls = (bindings, howlDict, IDs) => {
+    for(let bind in bindings){
+        howlDict[bind] = new Howl({
+            src:['sounds/' + keybindings[bind] + '.mp3'],
+            html5: true
+        })
+        IDs[bind] = -1
+    }
+}
 
 
+let keybindings = {
+    'z': 'e',
+    'x': 'f',
+    'c': 'fsharp',
+    'v': 'g',
+    'b': 'gsharp',
+    'a': 'a',
+    's': 'asharp',
+    'd': 'b',
+    'f': 'c',
+    'g': 'csharp',
+    'h': 'd',
+}
 
 
-let x = new Howl({
-	src: ['sounds/e.mp3'],
-	html5: true
-})
-
+const defaults = JSON.stringify(keybindings)
+let howls = {}
+let howlIDs = {}
 let pushed = {}
-let currentlyPlaying = -1
+setHowls(keybindings, howls, howlIDs)
+
+// if there is a sound playing, 
+//  
+
+
 
 document.addEventListener("keydown", event => {
-    if(pushed[event.key] != true){
+    if(event.key in keybindings && !pushed[event.key]){
+      /*  currentHowl = howls[event.key]
+        currentlyPlayingID = currentHowl.play() */
+        howlIDs[event.key] = howls[event.key].play()
         pushed[event.key] = true
-        currentlyPlaying = x.play()
     }
 })
 
 document.addEventListener("keyup", event => {
-    pushed[event.key] = false
-    x.stop(currentlyPlaying)
+    if(event.key in keybindings){
+        pushed[event.key] = false
+        howls[event.key].stop(howlIDs[event.key])
+    }
 })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// first row: E F F# G g#
+// second row: A A# B C C# D
 
 
 
